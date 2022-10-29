02e2c2c89e1cbc7deb3fc72b05fa76e678f879cf
Lichyo <chyoxuan@gmail.com>
2022-10-29 23:17:01 +0800

    Bank Class Testing.

---
+++ Check/check.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class check
+{
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int x1;
+
+        x1 = scn.nextInt();
+        System.out.println(x1);
+        scn.close();
+        
+    }
+}
---
+++ Class_Practice/classify.java
@@ -0,0 +1,31 @@
+package Class_Practice;
+
+class Bank
+{
+    int account;
+    int balance;
+
+    void printInfo()
+    {
+        System.out.printf("Account = %d, Balance = %d\n", account, balance);
+    }
+}
+
+public class classify {
+    public static void main(String [] args)
+    {
+        Bank cindy = new Bank();
+        cindy.account = 111012048;
+        cindy.balance = 0;
+
+        Bank lichyo = new Bank();
+        lichyo.account = 111016041;
+        lichyo.balance = 100000;
+
+        System.out.printf("lichyo : ");
+        lichyo.printInfo();
+        
+        System.out.printf("Cindy : ");
+        cindy.printInfo();
+    }
+}
